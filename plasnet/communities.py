e34a021463a8cd57e66b461e64bbf3265f282607
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-10-26 20:53:52 +0100

    Adding Communities class (refactoring needed)

---
+++ plasnet/communities.py
@@ -0,0 +1,19 @@
+from typing import List
+from plasnet.community_graph import CommunityGraph
+import pickle
+
+
+class Communities(List[CommunityGraph]):
+    """
+    Represents plasmid communities
+    """
+
+    def save(self, communities_filepath):
+        with open(communities_filepath, "wb") as fh:
+            pickle.dump(self, fh)
+
+    @staticmethod
+    def load(communities_filepath) -> "Communities":
+        with open(communities_filepath, "rb") as fh:
+            communities = pickle.load(fh)
+            return communities

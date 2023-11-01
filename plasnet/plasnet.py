09fc9e81ea329af81b02e1434ad7cb74c45fa871
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-11-01 14:15:48 +0000

    Updating CLI

--- plasnet/plasnet.py
+++ plasnet/plasnet.py
@@ -8,12 +8,27 @@ from plasnet.output_producer import OutputProducer
 @click.command(epilog="""
 \b
 Creates and split a plasmid graph into communities.
-The plasmid graph is defined by a plasmid distances file.
+The plasmid graph is defined by plasmid and distance files.
+
+\b
+The plasmid file is a tab-separated file with one column describing all plasmids in the dataset.
+Example of such file:
+plasmid
+AP024796.1
+AP024825.1
+CP012142.1
+CP014494.1
+CP019149.1
+CP021465.1
+CP022675.1
+CP024687.1
+CP026642.1
+CP027485.1
+
+\b
 The distances file is a tab-separated file with 3 columns: plasmid_1, plasmid_2, distance.
 plasmid_1 and plasmid_2 are plasmid names, and distance is a float between 0 and 1.
 The distance threshold is the minimum distance value for two plasmids to be considered connected.
-
-\b
 Example of such file:
 plasmid_1       plasmid_2       distance
 AP024796.1      AP024825.1      0.8
@@ -26,13 +41,15 @@ AP024796.1      CP024687.1      0.0
 AP024796.1      CP026642.1      0.5
 AP024796.1      CP027485.1      0.8
 """)
+@click.argument("plasmids", type=PathlibPath(exists=True))
 @click.argument("distances", type=PathlibPath(exists=True))
 @click.argument("output-dir", type=PathlibPath(exists=False))
 @click.option("--distance-threshold", "-d", type=float, default=0.5, help="Distance threshold")
 @click.option("--bh-connectivity", "-b", type=int, default=10, help="Minimum number of connections a plasmid need to be considered a blackhole plasmid")
 @click.option("--bh-neighbours-edge-density", "-e", type=float, default=0.2, help="Maximum number of edge density between blackhole plasmid neighbours to label the plasmid as blackhole")
 @click.option("--output-plasmid-graph", "-p", is_flag=True, help="Also outputs the full, unsplit, plasmid graph")
-def split(distances: Path,
+def split(plasmids: Path,
+          distances: Path,
           output_dir: Path,
           distance_threshold: float,
           bh_connectivity: int,

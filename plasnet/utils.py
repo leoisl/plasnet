a716f5f2b8249c6fd6eb88f02fcc3c059a27692c
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-11-01 16:55:29 +0000

    Adding utils.distance_df_to_dict()

--- plasnet/utils.py
+++ plasnet/utils.py
@@ -13,3 +13,11 @@ def get_libs_dir():
 class PathlibPath(click.Path):
     def convert(self, value, param, ctx):
         return Path(super().convert(value, param, ctx))
+
+
+def distance_df_to_dict(df):
+    df_as_dict = {}
+    for _, row in df.iterrows():
+        df_as_dict[(row["plasmid_1"], row["plasmid_2"])] = row["distance"]
+        df_as_dict[(row["plasmid_2"], row["plasmid_1"])] = row["distance"]
+    return df_as_dict

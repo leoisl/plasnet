df4ef4e5442afb93e363331f72789e66cde5fa78
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-11-06 14:44:46 +0000

    Improving type hinting in utils.py

--- plasnet/utils.py
+++ plasnet/utils.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 import click
+import pandas as pd
 
 
-def get_plasnet_source_dir():
+def get_plasnet_source_dir() -> Path:
     return Path(__file__).parent
 
 
-def get_libs_dir():
+def get_libs_dir() -> Path:
     return get_plasnet_source_dir() / "ext/libs"
 
 
@@ -15,7 +16,10 @@ class PathlibPath(click.Path):
         return Path(super().convert(value, param, ctx))
 
 
-def distance_df_to_dict(df):
+DistanceDict = dict[tuple[str, str], float]
+
+
+def distance_df_to_dict(df: pd.DataFrame) -> DistanceDict:
     df_as_dict = {}
     for _, row in df.iterrows():
         df_as_dict[(row["plasmid_1"], row["plasmid_2"])] = row["distance"]

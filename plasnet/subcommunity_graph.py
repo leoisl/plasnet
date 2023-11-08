45e18b445f200dfde2d18dc70025ea7f1074cd02
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-11-08 14:53:47 +0000

    fix: Improving SubcommunityGraph inheritance and implementing required methods

--- plasnet/subcommunity_graph.py
+++ plasnet/subcommunity_graph.py
@@ -1,2 +1,10 @@
-class SubcommunityGraph:
-    ...
+from plasnet.blackhole_graph import BlackholeGraph
+
+
+class SubcommunityGraph(BlackholeGraph):
+    def _get_libs_relative_path(self) -> str:
+        return ".."
+
+    def _get_samples_selectors_HTML(self) -> str:
+        return ""
+

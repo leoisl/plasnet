9062ca266b1d927dfadb01a6d14ac8b3d8682c30
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-11-17 15:24:38 +0000

    refactor: ListOfSampleGraphs -> SampleGraphs

--- plasnet/sample_graphs.py
+++ plasnet/sample_graphs.py
@@ -2,8 +2,8 @@ from plasnet.list_of_graphs import ListOfGraphs
 from plasnet.sample_graph import SampleGraph
 
 
-class ListOfSampleGraphs(ListOfGraphs[SampleGraph]):
-    def get_graphs_sorted_by_size(self) -> "ListOfSampleGraphs":
+class SampleGraphs(ListOfGraphs[SampleGraph]):
+    def get_graphs_sorted_by_size(self) -> "SampleGraphs":
         nb_of_hit_samples = map(SampleGraph.get_number_of_hit_samples, self)
 
         graphs_and_nb_of_hit_samples = [
@@ -14,4 +14,4 @@ class ListOfSampleGraphs(ListOfGraphs[SampleGraph]):
             graphs_and_nb_of_hit_samples, key=lambda x: x[1], reverse=True
         )
         sorted_graphs = [graph for graph, _ in graphs_and_nb_of_hit_samples_sorted]
-        return ListOfSampleGraphs(sorted_graphs)
+        return SampleGraphs(sorted_graphs)
